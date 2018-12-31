f4f76f24b2de807a46eab0df031177d687adea27
mrmckracken <lutz.tobias@gmail.com>
2018-12-31 15:54:16 +0100

    finished https://youtu.be/D-XzAeVvMkg

--- sketch.js
+++ sketch.js
@@ -3,13 +3,14 @@ function setup() {
     noCanvas();
     const values = [];
     console.log("start");
-    for (let i = 0; i<15; i++){
+    for (let i = 0; i<30; i++){
         values[i]= random(0,100);
 
     }
 
-    const shape = [5,3];
-    const tensor1 = tf.tensor(values, shape);
+    const shape = [2,5,3];
+    //const tensor1 = tf.tensor(values, shape,'int32');
+    const tensor1 = tf.tensor3d(values, shape,'int32');
 
     console.log (tensor1.toString());
     console.log (tensor1);
