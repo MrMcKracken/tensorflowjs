21e06e00c31b9fd43f93793dcbf1c4334f594e84
mrmckracken <lutz.tobias@gmail.com>
2019-01-05 16:37:04 +0100

    Chapter 3 matrix mul

--- sketch.js
+++ sketch.js
@@ -1,17 +1,34 @@
 
+function getTime(){
+    var currentdate = new Date(); 
+     return  "["+currentdate.getHours() + ": " 
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds()+"s]";
+}
+
+
 function setup() {
     noCanvas();
     const values = [];
-    console.log("start");
+    console.log(getTime()+" Start");
     for (let i = 0; i<30; i++){
         values[i]= random(0,100);
 
     }
-
+//    const vTensor1 tf.variable(tensor1);
     const shape = [2,5,3];
-    //const tensor1 = tf.tensor(values, shape,'int32');
-    const tensor1 = tf.tensor3d(values, shape,'int32');
-
-    console.log (tensor1.toString());
-    console.log (tensor1);
+    const tensorA = tf.tensor3d(values, shape,'int32');
+    const tensorB = tf.tensor3d(values, shape,'int32');
+    
+    const tensorC=tensorB.add(tensorA);
+    
+    
+    
+    console.log (tensorA.toString());
+    console.log (tensorC.toString());
+    //console.log(tensor1.dataSync();
+    //console.log(tensor1.get(0,0,0));
+    //    console.log.(vTensor1);
+    
+    
 }
