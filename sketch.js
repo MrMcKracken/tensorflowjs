1c4e40fcfc7c06724f80a18f6b080cdfff3ba701
mrmckracken <lutz.tobias@gmail.com>
2019-01-05 17:04:11 +0100

    Chapter 3: finished. matrix mul

--- sketch.js
+++ sketch.js
@@ -11,21 +11,28 @@ function setup() {
     noCanvas();
     const values = [];
     console.log(getTime()+" Start");
-    for (let i = 0; i<30; i++){
+    for (let i = 0; i<15; i++){
         values[i]= random(0,100);
 
     }
 //    const vTensor1 tf.variable(tensor1);
-    const shape = [2,5,3];
-    const tensorA = tf.tensor3d(values, shape,'int32');
-    const tensorB = tf.tensor3d(values, shape,'int32');
+    const shapeA = [5,3];
+    const shapeB = [3,5];
     
-    const tensorC=tensorB.add(tensorA);
     
+    
+    const tensorA = tf.tensor2d(values, shapeA,'int32');
+    const tensorB = tf.tensor2d(values, shapeB,'int32');
+    
+    // equal results 
+//    const tensorC=tensorA.matMul(tensorB);
+    const tensorC=tensorA.matMul(tensorA.transpose());
     
     
     console.log (tensorA.toString());
-    console.log (tensorC.toString());
+    console.log (tensorB.toString());
+    
+    tensorC.print();
     //console.log(tensor1.dataSync();
     //console.log(tensor1.get(0,0,0));
     //    console.log.(vTensor1);
