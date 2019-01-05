9f65aeabf28e00a17bc14637242ab830cbbbcf8e
mrmckracken <lutz.tobias@gmail.com>
2019-01-05 17:27:00 +0100

    Chapter 4 memory leaks with draymethod

--- sketch.js
+++ sketch.js
@@ -9,33 +9,34 @@ function getTime(){
 
 function setup() {
     noCanvas();
+//    frameRate(1);
+}
+
+tf.tidy(myFunc);
+
+function myFunc(){
+    
+}
+
+function draw(){
     const values = [];
     console.log(getTime()+" Start");
-    for (let i = 0; i<15; i++){
+    for (let i = 0; i<150000; i++){
         values[i]= random(0,100);
 
     }
-//    const vTensor1 tf.variable(tensor1);
-    const shapeA = [5,3];
-    const shapeB = [3,5];
+    const shape = [500,300];
+    const tensorA = tf.tensor2d(values, shape,'int32');
+    const tensorB = tf.tensor2d(values, shape,'int32');
+    const tensorBTrans = tensorB.transpose();
+    const tensorC=tensorA.matMul(tensorBTrans);
+
+//    tedious way...
+//    tensorA.dispose();
+//    tensorB.dispose();
+//    tensorC.dispose();
+//    tensorBTrans.dispose();
     
-    
-    
-    const tensorA = tf.tensor2d(values, shapeA,'int32');
-    const tensorB = tf.tensor2d(values, shapeB,'int32');
-    
-    // equal results 
-//    const tensorC=tensorA.matMul(tensorB);
-    const tensorC=tensorA.matMul(tensorA.transpose());
-    
-    
-    console.log (tensorA.toString());
-    console.log (tensorB.toString());
-    
-    tensorC.print();
-    //console.log(tensor1.dataSync();
-    //console.log(tensor1.get(0,0,0));
-    //    console.log.(vTensor1);
-    
+    console.log(tf.memory().numTensors);
     
 }
